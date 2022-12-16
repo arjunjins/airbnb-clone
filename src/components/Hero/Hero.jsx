080cd926d9ce604da2d2ed2317f2b66275358330
Arjun J <arjunjins2001@gmail.com>
2022-12-16 14:28:09 +0530

    hero section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,13 @@ const Hero = () => {
   return (
     <div className="hero">
       <img src={pics} alt="" />
+      <div className="hero-text">
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
     </div>
   );
 };
